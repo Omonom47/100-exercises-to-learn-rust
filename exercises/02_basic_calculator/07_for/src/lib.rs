54ca5ef53a041564c15434981f53ba14cf51069e
Maxime Havez <56996234+Omonom47@users.noreply.github.com>
2024-10-23 22:18:05 +0200

    solved up to and including 4.7

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,10 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut fac = 1;
+    for i in 0..n  {
+        fac = fac * (n-i);
+    }
+    fac
 }
 
 #[cfg(test)]
--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,68 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order{
+    product_name: String,
+    quantity: u32,
+    unit_price: u32
+}
+
+fn verify_name(name: &String){
+    if name.is_empty() {
+        panic!("product_name may not be empty");
+    }
+    if name.len() > 300 {
+        panic!("product_name must be at most 300 bytes")
+    }
+}
+fn verify_quantity(quantity:&u32) {
+    if *quantity == 0 {
+        panic!("quantity must be greater than 0");
+    }
+}
+fn verify_price(price:&u32) {
+    if *price == 0 {
+        panic!("unit_price must be greater than 0");
+    }
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Order{
+        verify_name(&product_name);
+        verify_quantity(&quantity);
+        verify_price(&unit_price);
+        Order{
+            product_name,
+            quantity,
+            unit_price
+        }
+    }
+
+    pub fn product_name(&self) ->&String{
+        &self.product_name
+    }
+    pub fn quantity(&self) ->&u32{
+        &self.quantity
+    }
+    pub fn unit_price(&self) ->&u32{
+        &self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, new_name: String){
+        verify_name(&new_name);
+        self.product_name = new_name;
+    }
+    pub fn set_quantity(&mut self, new_quantity: u32){
+        verify_quantity(&new_quantity);
+        self.quantity = new_quantity;
+    }
+    pub fn set_unit_price(&mut self, new_price: u32){
+        verify_price(&new_price);
+        self.unit_price = new_price
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+}
